c078ef3c87340ca8525e64c8703757332fa0a6d3
Nallabothulabhanuvardhan <bvardhan75@gmail.com>
2025-06-12 01:54:19 +0530

    Day 10 of javascript

---
+++ day10.js
@@ -0,0 +1,32 @@
+console.log("===ORDER SUMMARY===");
+
+let restaurentName ="Domino's Pizza";
+
+let Cname ="Raj patel";
+
+let itemName = "margherita pizza";
+
+let Quantity = 2;
+
+let price = 299;
+
+let total =price * Quantity
+
+let isDelivery = "Yes"
+
+console.log(" Restaurant:"+restaurentName);
+
+console.log("Customer :"+Cname);
+
+console.log(" item:"+ itemName);
+
+console.log(" Quantity:"+Quantity);
+
+console.log(" Price per Item:"+price);
+
+console.log(" Total :"+total);
+
+console.log(" Delivery needed:"+isDelivery);
+
+
+
